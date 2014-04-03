92fd6a19babc8370a9cab41af0764e0bb4f1eade
triplex148 <triplex148@gmail.com>
2014-04-03 20:02:43 +0200

    changed identifier to naming conventions

--- database/dbscripts/event_Tabs_2014.sql
+++ database/dbscripts/event_Tabs_2014.sql
@@ -2,6 +2,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 DROP TABLE IF EXISTS `SENTIMENT`;
 DROP TABLE IF EXISTS `FILT_COLUMN`;
+DROP TABLE IF EXISTS `FILT_CONSIDER_TWEET`;
+DROP TABLE IF EXISTS `FILT_IGNORE_TWEET`;
 DROP TABLE IF EXISTS `TWEET_FILTER`;
 DROP TABLE IF EXISTS `TWEET_TAG`;
 DROP TABLE IF EXISTS `TWEET_ENTRIES`;
@@ -9,59 +11,59 @@ DROP TABLE IF EXISTS `EVENT`;
 
 # Event-Table where all events are saved
 CREATE TABLE IF NOT EXISTS `EVENT` (
-  `EVENT_ID` int(4) NOT NULL AUTO_INCREMENT,
-  `EVENTNAME` varchar(100) NOT NULL,
-  `EVENTDESCRIPTION` varchar(1000) NOT NULL,
-  `STARTDATE` date DEFAULT NULL,
-  `ENDDATE` date DEFAULT NULL,
-  `TWEETCOUNT` int(6) DEFAULT NULL,
-  `EVENTSTATE` char(1) DEFAULT '0', #0 Initial ; 1 Sammlung aktiv ; 2 Sammlung beendet
+  `event_id` int(4) NOT NULL AUTO_INCREMENT,
+  `event_title` varchar(100) NOT NULL,
+  `event_description` varchar(1000) NOT NULL,
+  `event_start_date` date DEFAULT NULL,
+  `event_end_date` date DEFAULT NULL,
+  `event_tw_count` int(6) DEFAULT NULL, #tweet count
+  `event_state` char(1) DEFAULT '0', #0 Initial ; 1 Sammlung aktiv ; 2 Sammlung beendet
   PRIMARY KEY (`EVENT_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-INSERT INTO `EVENT` (`EVENT_ID`, `EVENTNAME`, `EVENTDESCRIPTION`, `STARTDATE`, `ENDDATE`, `TWEETCOUNT`) VALUES
+INSERT INTO `EVENT` (`event_id`, `event_title`, `event_description`, `event_start_date`, `event_end_date`, `event_tw_count`) VALUES
 (1, 'Spielberg 2014', 'Eine kurze Beschreibung zum Spielberg Event', '2014-05-24', '2014-07-01', 100);
-INSERT INTO `EVENT` (`EVENT_ID`, `EVENTNAME`, `EVENTDESCRIPTION`, `STARTDATE`, `ENDDATE`, `TWEETCOUNT`) VALUES
+INSERT INTO `EVENT` (`event_id`, `event_title`, `event_description`, `event_start_date`, `event_end_date`, `event_tw_count`) VALUES
 (2, 'Theater Mamma Mia 2014', 'Eine kurze Beschreibung zum MAMA MIA Event', '2014-10-01', '2014-12-24', 100);
 
 # Tweet Tags, which will be considered at collection time. These tags belongs to one event. At least one tag must exists for each event.
 CREATE TABLE IF NOT EXISTS `TWEET_TAG` (
-  `TAG_ID` int(4) NOT NULL AUTO_INCREMENT,
-  `TAGNAME` varchar(50) NOT NULL,
-  `EVENT_ID` int(4),
-  PRIMARY KEY (`TAG_ID`),
-  FOREIGN KEY (`EVENT_ID`) REFERENCES `EVENT` (`EVENT_ID`)
+  `tag_id` int(4) NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(50) NOT NULL,
+  `event_id` int(4),
+  PRIMARY KEY (`tag_id`),
+  FOREIGN KEY (`event_id`) REFERENCES `EVENT` (`event_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
-INSERT INTO `TWEET_TAG` (`TAG_ID`, `TAGNAME`, `EVENT_ID`) VALUES (1, 'Spielberg', 1);
-INSERT INTO `TWEET_TAG` (`TAG_ID`, `TAGNAME`, `EVENT_ID`) VALUES (2, 'F1', 1);
-INSERT INTO `TWEET_TAG` (`TAG_ID`, `TAGNAME`, `EVENT_ID`) VALUES (3, 'Vettel', 1);
-INSERT INTO `TWEET_TAG` (`TAG_ID`, `TAGNAME`, `EVENT_ID`) VALUES (4, 'Abba', 2);
-INSERT INTO `TWEET_TAG` (`TAG_ID`, `TAGNAME`, `EVENT_ID`) VALUES (5, 'Mama Mia', 2);
+INSERT INTO `TWEET_TAG` (`tag_id`, `tag_name`, `event_id`) VALUES (1, 'Spielberg', 1);
+INSERT INTO `TWEET_TAG` (`tag_id`, `tag_name`, `event_id`) VALUES (2, 'F1', 1);
+INSERT INTO `TWEET_TAG` (`tag_id`, `tag_name`, `event_id`) VALUES (3, 'Vettel', 1);
+INSERT INTO `TWEET_TAG` (`tag_id`, `tag_name`, `event_id`) VALUES (4, 'Abba', 2);
+INSERT INTO `TWEET_TAG` (`tag_id`, `tag_name`, `event_id`) VALUES (5, 'Mama Mia', 2);
 
 # Tweet entries per Event
 CREATE TABLE IF NOT EXISTS `TWEET_ENTRIES` (
-  `TWEET_ID` int(8) NOT NULL AUTO_INCREMENT,
-  `TWEETTEXT` varchar(4000) NOT NULL,
-  `CREATIONDATE` date NOT NULL,
-  `USERNAME` varchar(50) NOT NULL,
-  `LOCATION` varchar(100) DEFAULT NULL,
-  `LANGUAGE` char(2) DEFAULT NULL,
-  `DELETED` char(1) DEFAULT '0',
-  `EVENT_ID` int(4),
-  PRIMARY KEY (`TWEET_ID`),
-  FOREIGN KEY (`EVENT_ID`) REFERENCES `EVENT` (`EVENT_ID`)
+  `tweet_id` int(8) NOT NULL AUTO_INCREMENT,
+  `tw_text` varchar(4000) NOT NULL,
+  `tw_creationdate` date NOT NULL,
+  `tw_user` varchar(50) NOT NULL,
+  `tw_location` varchar(100) DEFAULT NULL,
+  `tw_language` char(2) DEFAULT NULL,
+  `tw_deleted` char(1) DEFAULT '0',
+  `event_id` int(4),
+  PRIMARY KEY (`tweet_id`),
+  FOREIGN KEY (`event_id`) REFERENCES `EVENT` (`event_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
-INSERT INTO `TWEET_ENTRIES` (`TWEET_ID`, `TWEETTEXT`, `CREATIONDATE`, `USERNAME`, `LOCATION`, `LANGUAGE`, `EVENT_ID`) VALUES
+INSERT INTO `TWEET_ENTRIES` (`tweet_id`, `tw_text`, `tw_creationdate`, `tw_user`, `tw_location`, `tw_language`, `event_id`) VALUES
 (1, 'Well done - got the ticket for Spielberg GP 2014', '2014-05-05', 'Micael Steward', 'München', 'at', 1);
-INSERT INTO `TWEET_ENTRIES` (`TWEET_ID`, `TWEETTEXT`, `CREATIONDATE`, `USERNAME`, `LOCATION`, `LANGUAGE`, `EVENT_ID`) VALUES
+INSERT INTO `TWEET_ENTRIES` (`tweet_id`, `tw_text`, `tw_creationdate`, `tw_user`, `tw_location`, `tw_language`, `event_id`) VALUES
 (2, 'I love Steven Spielberg', '2014-04-03', 'Doris Hart', 'Klagenfurt', 'at', 1);
 
 CREATE TABLE IF NOT EXISTS `TWEET_FILTER` (
-  `FILTER_ID` int(4) NOT NULL AUTO_INCREMENT,
-  `DESCRIPTION` varchar(100) NOT NULL,
-  `CONSIDER_TWEET` varchar(100) DEFAULT NULL,
+  `filter_id` int(4) NOT NULL AUTO_INCREMENT,
+  `fi_description` varchar(100) NOT NULL,
+  `fi_consider_tweet` varchar(100) DEFAULT NULL,
   `IGNORE_TWEET` varchar(100) DEFAULT NULL,
   `TIME_START` date NOT NULL,
   `TIME_END` date NOT NULL,
@@ -70,20 +72,38 @@ CREATE TABLE IF NOT EXISTS `TWEET_FILTER` (
   FOREIGN KEY (`EVENT_ID`) REFERENCES `EVENT` (`EVENT_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
+# All tweet descriptions in this table will be ignored for the filter
+CREATE TABLE IF NOT EXISTS `FILT_IGNORE_TWEET` (
+  `filt_ignore_id` int(4) NOT NULL AUTO_INCREMENT,
+  `ignored_tweet` varchar(100) DEFAULT NULL,
+  `filter_id` int(4),
+  PRIMARY KEY (`filt_ignore_id`),
+  FOREIGN KEY (`filter_id`) REFERENCES `TWEET_FILTER` (`filter_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4;
+
+# All tweet descriptions in this table will be considered for the filter
+CREATE TABLE IF NOT EXISTS `FILT_CONSIDER_TWEET` (
+  `filt_consider_id` int(4) NOT NULL AUTO_INCREMENT,
+  `consider_tweet` varchar(100) DEFAULT NULL,
+  `filter_id` int(4),
+  PRIMARY KEY (`filt_consider_id`),
+  FOREIGN KEY (`filter_id`) REFERENCES `TWEET_FILTER` (`filter_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4;
+
 # All tweets in this table will be hide
 CREATE TABLE IF NOT EXISTS `FILT_COLUMN` (
-  `FILT_COL_ID` int(4) NOT NULL AUTO_INCREMENT,
-  `FILTER_ID` int(4),
-  `TWEET_ID` int(8),
-  PRIMARY KEY (`FILT_COL_ID`),
-  FOREIGN KEY (`FILTER_ID`) REFERENCES `TWEET_FILTER` (`FILTER_ID`),
-  FOREIGN KEY (`TWEET_ID`) REFERENCES `TWEET_ENTRIES` (`TWEET_ID`)
+  `filt_column_id` int(4) NOT NULL AUTO_INCREMENT,
+  `filter_id` int(4),
+  `tweet_id` int(8),
+  PRIMARY KEY (`filt_column_id`),
+  FOREIGN KEY (`filter_id`) REFERENCES `TWEET_FILTER` (`filter_id`),
+  FOREIGN KEY (`tweet_id`) REFERENCES `TWEET_ENTRIES` (`tweet_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4;
 
 CREATE TABLE IF NOT EXISTS `SENTIMENT` (
-  `SENT_ID` int(8) NOT NULL AUTO_INCREMENT,
-  `SENT_TEXT` varchar(50) NOT NULL,
-  `SENT_WEIGHT` int(1) NOT NULL,
-  PRIMARY KEY (`SENT_ID`)
+  `sent_id` int(8) NOT NULL AUTO_INCREMENT,
+  `sent_text` varchar(50) NOT NULL,
+  `sent_weight` int(1) NOT NULL,
+  PRIMARY KEY (`sent_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6;
 
