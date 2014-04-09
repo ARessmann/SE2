0374344b9dd9fd065614477a5d0ec4e17bb1269f
maxkr <maximilian.krumpholz@gmail.com>
2014-04-09 13:37:41 +0200

    Erster Teil der Unit tests (hauptsächlich getter/setter)

--- test/EventTest.php
+++ test/EventTest.php
@@ -2,7 +2,7 @@
 
 class EventTest extends PHPUnit_Framework_TestCase
 {
-	//for example
+	
     public function testExceptionIsRaisedForInvalidConstructorArguments()
     {
         new Core_Model_Event('1');
@@ -12,10 +12,80 @@ class EventTest extends PHPUnit_Framework_TestCase
     {
     	$event = new Core_Model_Event();
     	
-    	//... add attributes
-    	
+    	$event->setEventTitle("Test");
+    	$event->setEventDescription("Test");
+    	$event->setEventFrom("01.01.2000");
+    	$event->setEventTo("02.02.2002");
+    	$event->setEventTwCount("1");
+    	$event->setEventState("running");
+    	$event->setEventTweetTags("TestTag");
+    	    	
     	$id = $event->save();
     	
     	$this->assertTrue(isset($id));
+    }
+    
+    
+    public function testSetGetEventTitle()
+    {
+       	$event = new Core_Model_Event();
+       	
+       	$event->setEventTitle("test");
+    
+    	$this->assertEqual("test", $event->getEventTitle);
+    }
+    
+    public function testSetGetEventDescription()
+    {
+    	$event = new Core_Model_Event();
+    
+    	$event->setEventDescription("test");
+    
+    	$this->assertEqual("test", $event->getEventDescription);
+    }
+    
+    public function testSetGetEventTo()
+    {
+    	$event = new Core_Model_Event();
+    
+    	$event->setEventTo(01.01.2000);
+    
+    	$this->assertEqual(01.01.2000, $event->getEventTo);
+    }
+    
+    public function testSetGetEventFrom()
+    {
+    	$event = new Core_Model_Event();
+    
+    	$event->setEventFrom(01.01.2000);
+    
+    	$this->assertEqual(01.01.2000, $event->getEventFrom);
+    }
+    
+    public function testSetGetEventTwCount()
+    {
+    	$event = new Core_Model_Event();
+    
+    	$event->setEventTwCount("1");
+    
+    	$this->assertEqual("1", $event->getEventTwCount);
+    }
+    
+    public function testSetGetEventState()
+    {
+    	$event = new Core_Model_Event();
+    
+    	$event->setEventState("running");
+    
+    	$this->assertEqual("running", $event->getEventState);
+    }
+    
+    public function testSetGetTweetTags()
+    {
+    	$event = new Core_Model_Event();
+    
+    	$event->setTweetTags("running");
+    
+    	$this->assertEqual("running", $event->getTweetTags);
     }
 }
