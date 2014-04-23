8e13508c265c151731afaa9df3aafc84d03c91ed
triplex148 <triplex148@gmail.com>
2014-04-23 16:34:54 +0200

    tweet entry primary key got 20 signs long

--- database/dbscripts/event_Tabs_2014.sql
+++ database/dbscripts/event_Tabs_2014.sql
@@ -18,14 +18,16 @@ CREATE TABLE IF NOT EXISTS `EVENT` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-INSERT INTO `EVENT` (`id`, `event_title`, `event_description`, `event_from`, `event_to`, `event_tw_count`) VALUES
-(1, 'Spielberg 2014', 'Eine kurze Beschreibung zum Spielberg Event', '2014-05-24', '2014-07-01', 100);
-INSERT INTO `EVENT` (`id`, `event_title`, `event_description`, `event_from`, `event_to`, `event_tw_count`) VALUES
-(2, 'Theater Mamma Mia 2014', 'Eine kurze Beschreibung zum MAMA MIA Event', '2014-10-01', '2014-12-24', 100);
+INSERT INTO `EVENT` (`id`, `event_title`, `event_description`, `event_from`, `event_to`, `event_tw_count`, `event_tweet_tags`) VALUES
+(1, 'Spielberg 2014', 'Eine kurze Beschreibung zum Spielberg Event', '2014-04-20', '2014-07-01', 100, 'Spielberg,F1');
+INSERT INTO `EVENT` (`id`, `event_title`, `event_description`, `event_from`, `event_to`, `event_tw_count`, `event_tweet_tags`) VALUES
+(2, 'Theater Mamma Mia 2014', 'Eine kurze Beschreibung zum MAMA MIA Event', '2014-04-01', '2014-12-24', 10, 'Mamma Mia,Pierce Brosnan');
+INSERT INTO `EVENT` (`id`, `event_title`, `event_description`, `event_from`, `event_to`, `event_tw_count`, `event_tweet_tags`) VALUES
+(3, 'Fußball WM 2014 Brasil', 'WM 2014', '2014-04-01', '2014-07-01', 1000, 'WM,2014,Brasilien');
 
 -- Tweet entries for an event which are filled by a native java program
 CREATE TABLE IF NOT EXISTS `TWEET_ENTRY` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `tw_text` varchar(4000) NOT NULL,
   `tw_creationdate` date NOT NULL,
   `tw_user` varchar(50) NOT NULL,
@@ -37,15 +39,10 @@ CREATE TABLE IF NOT EXISTS `TWEET_ENTRY` (
   FOREIGN KEY (`event_id`) REFERENCES `EVENT` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
-INSERT INTO `TWEET_ENTRY` (`id`, `tw_text`, `tw_creationdate`, `tw_user`, `tw_location`, `tw_language`, `event_id`) VALUES
-(1, 'Well done - got the ticket for Spielberg GP 2014', '2014-05-05', 'Micael Steward', 'München', 'at', 1);
-INSERT INTO `TWEET_ENTRY` (`id`, `tw_text`, `tw_creationdate`, `tw_user`, `tw_location`, `tw_language`, `event_id`) VALUES
-(2, 'I love Steven Spielberg', '2014-04-03', 'Doris Hart', 'Klagenfurt', 'at', 1);
-
 -- Analysis table which tweet entries for an event should be considered for further processing
 CREATE TABLE IF NOT EXISTS `TWEET_FOR_ANALYSIS` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
-  `tweet_id` int(8) NOT NULL,
+  `tweet_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`tweet_id`) REFERENCES `TWEET_ENTRY` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4;
