f24836ecfe774698206d145d0e76f1a4c01be928
Johannes <Johannes@Notebook>
2014-05-15 21:10:51 +0200

    nur dieselbe datei von manfred noch einmal

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -139,4 +139,19 @@ class IndexController extends Core_AbstractController {
         
         $this->view->tweets = $this->getPagedElements($tweets);
     }
+    
+    /**
+     * display the list of sentiments
+     * and set the object to the view
+     */
+    public function sentimentAction () {
+    	$sentiment = new Core_Model_Sentiment();
+    
+    	$sentiments = $sentiment->loadAll ();
+        	
+    	$this->view->counter = count($sentiments);
+    	$this->view->menuOptions = $this->getMenu ();
+    
+    	$this->view->sentiments = $this->getPagedElements($sentiments);
+    }
 }
