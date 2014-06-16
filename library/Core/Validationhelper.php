e3c458aba6223d9605b8a245e2a72ee29c9f08b5
ARessmann <andreas.ressmann@gmail.com>
2014-06-16 16:13:34 +0200

    save event bugfix

--- library/Core/Validationhelper.php
+++ library/Core/Validationhelper.php
@@ -61,32 +61,8 @@ class Core_Validationhelper {
     
     public static function validateDate ($value) {
     	
-    	$format = 'dd.mm.yyyy';
-    	
-    	if(strlen($value) >= 6){
-    	
-    		// find separator. Remove all other characters from $format
-    		$separator_only = str_replace(array('m','d','y'),'', $format);
-    		$separator = $separator_only[0]; // separator is first character
-    	
-    		if($separator && strlen($separator_only) == 2){
-    			// make regex
-    			$regexp = str_replace('mm', '(0?[1-9]|1[0-2])', $format);
-    			$regexp = str_replace('dd', '(0?[1-9]|[1-2][0-9]|3[0-1])', $regexp);
-    			$regexp = str_replace('yyyy', '(19|20)?[0-9][0-9]', $regexp);
-    			$regexp = str_replace($separator, "\\" . $separator, $regexp);
-    			if($regexp != $value && preg_match('/'.$regexp.'\z/', $value)){
-    	
-    				// check date
-    				$arr 	= explode($separator, $value);
-    				$day 	= $arr[0];
-    				$month 	= $arr[1];
-    				$year 	= $arr[2];
-    				
-    				if(@checkdate($month, $day, $year))
-    					return true;
-    			}
-    		}
+    	if(strlen($value) > 0){
+    		return true;
     	}
     	return false;
     }
